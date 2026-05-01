511eb586cdba94baa6240354df1e96018fc6bd07
hammed <mederhoo@gmail.com>
2026-05-01 21:29:51 +0100

    update

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { type NextRequest, NextResponse } from 'next/server'
 import { updateSession } from '@/utils/supabase/middleware'
 
-export default async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
     if (request.nextUrl.pathname === '/') {
         return NextResponse.redirect(new URL('/login', request.url))
     }
